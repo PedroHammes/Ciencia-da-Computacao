87a82baf682bbf3a095d4cc852229e385b43d875
Pedro Hammes <81041208+PedroHammes@users.noreply.github.com>
2026-05-31 21:08:49 +0000

    resolução ex. 005

--- 3S/ED - Estrutura de Dados/estudos/01-fundamentos/005-algoritmos-de-busca.c
+++ 3S/ED - Estrutura de Dados/estudos/01-fundamentos/005-algoritmos-de-busca.c
@@ -1,3 +1,33 @@
 // 5. Algoritmos de Busca
-// Exercício: Escreva uma função de busca sequencial que percorra um vetor estático de inteiros e retorne 1 se encontrar o valor alvo, ou 0 caso não encontre. Explique como o algoritmo garante que todo o vetor seja verificado.
+// Exercício: Escreva uma função de busca sequencial que percorra um vetor estático de inteiros e retorne 1 se encontrar o valor alvo, ou 0 caso não encontre. 
+// Explique como o algoritmo garante que todo o vetor seja verificado.
 // C - Guia do Programador (Aitkens e Jones): Dia 8 (Matrizes/Vetores).
+
+// BIBLIOTECAS
+#include <stdio.h>
+
+// CONSTANTES
+#define MAX 12
+
+// VARIÁVEIS
+int target, matriz[MAX] = {0, 1, 4, 2, 10, 3, 9, 5, 6, 11, 21, 19};
+int search(int n, int array[], int size);
+
+int main ()
+{
+    printf("Informe o valor a ser buscado: \n");
+    scanf("%d", &target);
+
+    printf("%d \n", search(target, matriz, MAX));
+
+    return 0;
+}
+
+int search(int n, int array[], int size)
+{
+    for (int i=0; i < size; i++)
+    {
+        if (n == array[i]) { return 1; }
+    }
+    return 0;
+}
